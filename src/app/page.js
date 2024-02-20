7938b681f2e57269fac3e9c9629a9cf8ffea37cc
Bylander <joao.amb23@gmail.com>
2024-02-20 16:41:47 -0300

    add home search component style

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
 import HomeHeader from '@/components/HomeHeader'
-//import React from 'react'
+import HomeSearch from '@/components/HomeSearch'
+import React from 'react'
+import Image from 'next/image'
+import { TbHomeSearch } from 'react-icons/tb'
 
 export default function Home() {
   return (
     <>
-    <HomeHeader />
+      <HomeHeader />
+      <div className='flex flex-col items-center mt-24'>
+        <Image
+          src="https://logosmarcas.net/wp-content/uploads/2020/09/Google-Logo.png"
+          alt="Google Logo"
+          width={300}
+          height={100}
+        />
+        <HomeSearch />
+      </div>
     </>
   )
 }
