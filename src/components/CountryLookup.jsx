da26c07d3f8c4d6dda718738db4acbac29f68707
Bylander <joao.amb23@gmail.com>
2024-02-27 15:55:43 -0300

    add footer component

---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,20 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+
+export default function CountryLookup() {   
+    const [country, setCountry] = useState("Canada")
+ 
+    useEffect(() => {
+        const getCountry = async () => {
+            const response = await fetch("http://ip-api.com/json/")
+                .then((res) => res.json())
+                .then((data) => data.country)
+            if (!response) return
+            setCountry(response)
+        }
+        getCountry()
+    }, [])
+
+    return <div>{country}</div> 
+}
